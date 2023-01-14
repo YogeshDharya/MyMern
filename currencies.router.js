529d08c8e7b2bb253acf6dbf6b393daed5f7054a
YogeshDharya <76198944+YogeshDharya@users.noreply.github.com>
2023-01-14 21:33:26 +0530

    Add files via upload
    first express router

---
+++ currencies.router.js
@@ -0,0 +1,13 @@
+const data = require("../DB/users.json");
+const router = require('express').Router();
+
+router.get("/title",getCurrencyTitle)
+// const { getCurrencyTitle, getCurrencies, getCurrencyWithSymbol } = require('../controllers/currency.controller');
+
+// const router = require('express').Router();
+
+// router.get("/title", getCurrencyTitle); // currencies/title
+// router.get("/", getCurrencies); ///currencies
+// router.get("/:symbol", getCurrencyWithSymbol); //currencies/ANY_DYNAMIC_VALUE
+
+// module.exports = router;
