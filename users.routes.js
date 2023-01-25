68ea4d9e169126d0b5fca50d27591a1145da3ac0
YogeshDharya <76198944+YogeshDharya@users.noreply.github.com>
2023-01-25 18:28:50 +0530

    Update users.routes.js
    Not needed

--- users.routes.js
+++ users.routes.js
@@ -1,9 +1,1 @@
-// const { getAllUsers, getUsersByUuid, searchUsersByQuery } = require('../controllers/users.controller')
 
-// const router = require('express').Router();
-
-// router.get("/", getAllUsers);
-// router.get("/search", searchUsersByQuery);
-// router.get("/:uuid", getUsersByUuid);
-
-// module.exports = router
