c2523b9ab4fcb390438d339c55e77ac606808d9c
YogeshDharya <76198944+YogeshDharya@users.noreply.github.com>
2023-01-25 18:29:51 +0530

    Update currencies.routes.js
    Not needed

--- currencies.routes.js
+++ currencies.routes.js
@@ -1,9 +1,1 @@
-const { getCurrencyTitle, getCurrencies, getCurrencyWithSymbol } = require('../controllers/currency.controller');
 
-const router = require('express').Router();
-
-router.get("/title", getCurrencyTitle); // currencies/title
-router.get("/", getCurrencies); ///currencies
-router.get("/:symbol", getCurrencyWithSymbol); //currencies/ANY_DYNAMIC_VALUE
-
-module.exports = router;
