e93e182e2fa5e923cff27967aa5d03d6e94c82a8
YogeshDharya <76198944+YogeshDharya@users.noreply.github.com>
2023-01-31 22:48:31 +0530

    Update verifyAuth.js

--- verifyAuth.js
+++ verifyAuth.js
@@ -1,12 +1,1 @@
-const verifyAuth = (req, res, next) => {
-//     const { authorization } = req.headers;
-//     if (!authorization) {
-//         return res.status(403).json({ message: "Unauthorized Request" });
-//     }
-//     if (authorization !== PASSWORD) {
-//         return res.status(403).json({ message: "Unauthorized Request" });
-//     }
-//     next();
-// };
 
-// module.exports = { verifyAuth }
