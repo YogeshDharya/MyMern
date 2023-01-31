40ab8a08475719a74e43039478b1fac650f594c0
YogeshDharya <76198944+YogeshDharya@users.noreply.github.com>
2023-01-31 22:50:40 +0530

    Update blogs.model.js

--- blogs.model.js
+++ blogs.model.js
@@ -1,31 +1,31 @@
 const mongoose = require("mongoose");
-const validator = require("validator");
+// const validator = require("validator");
 
-const authorSchema = new mongoose.Schema(
-    {
-        fullName: { type: String, required: true, maxLength: 25 },
-        twitterHandle: { type: String },
-        email: {
-            type: String,
-            maxLength: 50,
-            required: true,
-            validate: (value) => validator.isEmail(value)
-        },
-        image: { type: String, validate: (value) => validator.isURL(value) },
-    },
-    { _id: false }
-);
+// const authorSchema = new mongoose.Schema(
+//     {
+//         fullName: { type: String, required: true, maxLength: 25 },
+//         twitterHandle: { type: String },
+//         email: {
+//             type: String,
+//             maxLength: 50,
+//             required: true,
+//             validate: (value) => validator.isEmail(value)
+//         },
+//         image: { type: String, validate: (value) => validator.isURL(value) },
+//     },
+//     { _id: false }
+// );
 
-const blogSchema = new mongoose.Schema(
-    {
-        title: { type: String, required: true, unique: true },
-        author: { type: [authorSchema] },
-        content: { type: String, default: "" },
-        publishedAt: { type: Date, default: null },
-    },
-    { timestamps: true }
-);
+// const blogSchema = new mongoose.Schema(
+//     {
+//         title: { type: String, required: true, unique: true },
+//         author: { type: [authorSchema] },
+//         content: { type: String, default: "" },
+//         publishedAt: { type: Date, default: null },
+//     },
+//     { timestamps: true }
+// );
 
-const blogModel = mongoose.model("Blogs", blogSchema);
+// const blogModel = mongoose.model("Blogs", blogSchema);
 
-module.exports = blogModel
+// module.exports = blogModel
