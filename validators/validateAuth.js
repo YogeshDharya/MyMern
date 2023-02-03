9fd01be8d4db9e4c1a9ec48852e783f139aac7e5
YogeshDharya <76198944+YogeshDharya@users.noreply.github.com>
2023-02-03 23:12:36 +0530

    Update validateAuth.js

--- validators/validateAuth.js
+++ validators/validateAuth.js
@@ -1,8 +1,14 @@
-const joi = require("joi");
-const schema = joi.object().keys({
-    age : joi.number().min(0).max(100),
-    gender : joi.string().valid("male","female")
-});
-const validateSearchQuery = (req,res)=>{
+const PASSWORD = process.env.PASSWORD
 
-}
+const verifyAuth = (req, res, next) => {
+    const { authorization } = req.headers;
+    if (!authorization) {
+        return res.status(403).json({ message: "Unauthorized Request" });
+    }
+    if (authorization !== PASSWORD) {
+        return res.status(403).json({ message: "Unauthorized Request" });
+    }
+    next();
+};
+
+module.exports = { verifyAuth }
