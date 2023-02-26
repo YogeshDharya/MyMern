b3a5a5be5623dcea28f993f8d4234a0f43e343a4
YogeshDharya <76198944+YogeshDharya@users.noreply.github.com>
2023-02-26 22:26:28 +0530

    Update users.validator.js

--- validators/users.validator.js
+++ validators/users.validator.js
@@ -1,15 +1,15 @@
 const Joi = require("joi");
-// const schema = Joi.object().keys({
-//     age: Joi.number().min(0).max(100),
-//     gender: Joi.string().valid("male", "female"),
-// });
-// const getQueryErrors = (req,res,next) => {
-//     const {age,gender}=req.query
-//     if(result){
-//         res.json(result)
-//     }else{
-//         res.error()
-//     }
-//  next();
-// }
-// module.exports = { getQueryErrors }
+const schema = Joi.object().keys({
+    age: Joi.number().min(0).max(100),
+    gender: Joi.string().valid("male", "female"),
+});
+const getQueryErrors = (req,res,next) => {
+    const {age,gender}=req.query
+    if(result){
+        res.json(result)
+    }else{
+        res.error()
+    }
+ next();
+}
+module.exports = { getQueryErrors }
