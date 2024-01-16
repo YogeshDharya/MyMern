6dee3bebc9e77e4e9f88bbe53f88c3dfafe202fa
YogeshDharya <76198944+YogeshDharya@users.noreply.github.com>
2024-01-16 23:53:15 +0530

    Update blogs.routes.js

--- routes/blogs.routes.js
+++ routes/blogs.routes.js
@@ -7,5 +7,5 @@ router.get("/search", searchBlogs); //api/blogs/search
 router.post("/new", createNewBlog); //api/blogs/new
 router.patch("/:id", updateBlog); //api/blogs/63bedc7dc0e67dfd26d0c1e7
 router.delete("/:id", deleteBlogWithId); //api/blogs/63bedc7dc0e67dfd26d0c1e7
-
+    
 module.exports = router;
